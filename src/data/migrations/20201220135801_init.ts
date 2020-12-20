5399b9eefefe0bf33f33d68c22eb3c596f1356b2
Robert Coe <chakrawc@gmail.com>
2020-12-20 15:20:35 -0800

    skip init if already eixsts

--- src/data/migrations/20201220135801_init.ts
+++ src/data/migrations/20201220135801_init.ts
@@ -1,12 +1,16 @@
 import * as Knex from "knex";
 
-export const up = async (knex: Knex): Promise<Knex.SchemaBuilder> =>
-	knex.schema.createTable("terrains", (table) => {
-		table.increments("id");
-		table.string("name");
-		table.unique(["name"]);
-		table.binary("data");
-	});
+export const up = async (knex: Knex): Promise<void> => {
+	const exists = await knex.schema.hasTable("terrains");
+
+	if (!exists)
+		await knex.schema.createTable("terrains", (table) => {
+			table.increments("id");
+			table.string("name");
+			table.unique(["name"]);
+			table.binary("data");
+		});
+};
 
 export const down = async (knex: Knex): Promise<Knex.SchemaBuilder> =>
 	knex.schema.dropTable("terrains");
