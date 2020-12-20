bb4872006acce619336bc94d73b6d50dc1e13a33
Robert Coe <chakrawc@gmail.com>
2020-12-20 10:34:32 -0800

    logging + start

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
+import morgan from "morgan";
 
 import { api } from "./api";
 import { port } from "./config";
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(api);
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`st2mr listening on http://localhost:${port}`);
 });
